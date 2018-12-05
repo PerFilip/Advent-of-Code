8b35da781a3c828d7183670a09039410a905cce9
PerFilip <filipheden@gmail.com>
2018-12-05 19:52:01 +0000

    5 complete (but slooow), starting day 4

---
+++ Day4/solution.py
@@ -0,0 +1,14 @@
+# falls or Guard
+import csv
+
+
+def solve_1():
+    sleep_time = dict()
+    with open('data.csv') as data:
+        reader = data.read()
+
+    return 1
+
+
+def solve_2():
+    return 1
