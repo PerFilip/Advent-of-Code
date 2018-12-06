08f1a63edca634caae533bfe13c85b889a944015
PerFilip <filipheden@gmail.com>
2018-12-06 14:29:48 +0000

    Day 1 done

--- Day1/solve.py
+++ Day1/solve.py
@@ -1,6 +1,6 @@
-import calibrate
+import solution
 
-a1 = str(calibrate.calculate())
+a1 = str(solution.calculate())
 print("The answear to puzzle 1 is: " + a1)
-a2 = str(calibrate.calculate(2))
+a2 = str(solution.calculate(2))
 print("The answear to puzzle 2 is: " + a2)
